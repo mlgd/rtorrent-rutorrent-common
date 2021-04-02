98f26ababb770d8ee0d67d6bdbef5156c3069bd4
mlgd <mlgd17@gmail.com>
2021-04-02 22:20:25 +0200

    Add env variables

--- startup/startup-rtorrent.sh
+++ startup/startup-rtorrent.sh
@@ -16,6 +16,8 @@ mkdir -p /downloads/.rtorrent/watch
 mkdir -p /downloads/.log/rtorrent
 if [ ! -e /downloads/.rtorrent/.rtorrent.rc ]; then
     cp /root/.rtorrent.rc /downloads/.rtorrent/
+    sed -i 's/network\.port_range.set *=.*$/network.port_range = '$INCOMING_PORT'/g' /downloads/.rtorrent/.rtorrent.rc
+    sed -i 's/dht\.port\.set *=.*$/dht.port.set = '$DHT_PORT'/g' /downloads/.rtorrent/.rtorrent.rc
 fi
 ln -s /downloads/.rtorrent/.rtorrent.rc /home/rtorrent/
 chown -R rtorrent:rtorrent /downloads/.rtorrent
@@ -26,4 +28,3 @@ rm -f /downloads/.rtorrent/session/rtorrent.lock
 
 # run
 su --login --command="TERM=xterm rtorrent" rtorrent 
-
