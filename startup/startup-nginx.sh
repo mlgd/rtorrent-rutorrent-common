25b301ddc651c36d2e9066a8d05c42689fcfd63f
mlgd <mlgd17@gmail.com>
2021-04-02 18:58:30 +0200

    Add startup files

---
+++ startup/startup-nginx.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+set -x
+
+chown -R www-data:www-data /var/www/rutorrent
+cp /downloads/.htpasswd /var/www/rutorrent/
+mkdir -p /downloads/.rutorrent/torrents
+chown -R www-data:www-data /downloads/.rutorrent
+mkdir -p /downloads/.log/nginx
+chown www-data:www-data /downloads/.log/nginx
+
+rm -f /etc/nginx/sites-enabled/*
+
+rm -rf /etc/nginx/ssl
+
+rm /var/www/rutorrent/.htpasswd
+
+
+# Basic auth enabled by default
+site=rutorrent-basic.nginx
+
+# Check if TLS needed
+if [ -e /downloads/nginx.key ] && [ -e /downloads/nginx.crt ]; then
+    mkdir -p /etc/nginx/ssl
+    cp /downloads/nginx.crt /etc/nginx/ssl/
+    cp /downloads/nginx.key /etc/nginx/ssl/
+    site=rutorrent-tls.nginx
+fi
+
+cp /root/$site /etc/nginx/sites-enabled/
+[ -n "$NOIPV6" ] && sed -i 's/listen \[::\]:/#/g' /etc/nginx/sites-enabled/$site
+[ -n "$WEBROOT" ] && ln -s /var/www/rutorrent /var/www/rutorrent/$WEBROOT
+
+# Check if .htpasswd presents
+if [ -e /downloads/.htpasswd ]; then
+    cp /downloads/.htpasswd /var/www/rutorrent/ && chmod 755 /var/www/rutorrent/.htpasswd && chown www-data:www-data /var/www/rutorrent/.htpasswd
+else
+# disable basic auth
+    sed -i 's/auth_basic/#auth_basic/g' /etc/nginx/sites-enabled/$site
+fi
+
+nginx -g "daemon off;"
+
---
+++ startup/startup-php.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -x
+
+MEM=${PHP_MEM:=256M}
+
+sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php/7.2/fpm/php.ini
+
+mkdir /run/php
+php-fpm7.2 --nodaemonize
+
